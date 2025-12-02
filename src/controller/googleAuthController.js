2e092bc7b675c06df28b300354fe739daedc3ff9
BintangAull <bintangokelah@gmail.com>
2025-12-02 14:56:28 +0700

    Initial commit dengan fitur yang sudah dikerjakan yaitu untuk service user meliputi , login , register , update , dan delete, serta setup lain nya seperti prisma orm versi 6 dan membuat struktur foldder agar lebih modular

---
+++ src/controller/googleAuthController.js
@@ -0,0 +1,61 @@
+import userService from "../service/googleAuth-service.js";
+import {prismaClient} from "../application/database.js";
+import {v4 as uuid} from "uuid";
+
+const googleAuthRedirect = (req, res, next) => {
+    try {
+        const redirect_uri = 'http://localhost:3000/auth/google/callback'
+        const client_id = process.env.GOOGLE_CLIENT_ID
+        const scope = encodeURIComponent('email profile')
+
+        const authURL = `https://accounts.google.com/o/oauth2/v2/auth?` +
+            `response_type=code&` +
+            `client_id=${client_id}&` +
+            `redirect_uri=${redirect_uri}&` +
+            `scope=${scope}&` +
+            `access_type=offline`
+
+
+        res.redirect(authURL)
+    }catch (e) {
+        next(e)
+    }
+}
+
+const googleAuthCallback = async (req, res, next) => {
+    try {
+        const code = req.query.code
+        const userData = await userService.handleGoogleCallback(code)
+        const { email, name, picture } = userData
+
+        //cek di database
+        let user = await prismaClient.user.findMany({
+            where: {
+                username: email,
+            }
+        })
+
+        if (!user){
+            const token = uuid().toString()
+            //kalo dia user gada maka kita insert ka database
+            user = await prismaClient.user.create({
+                data: {
+                    username : email,
+                    name : name,
+                    picture : picture, // ini kalo ada dan kita harus tanya dulu
+                    token : token
+                }
+            })
+        }
+
+        res.redirect('http://localhost:5173/dashboard') // redirect ke url front end kita
+    } catch (e) {
+        next(e);
+    }
+}
+
+export default {
+    googleAuthRedirect,
+    googleAuthCallback
+
+}
---
+++ src/route/api.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const userRouter = new express.Router();
+
+
+
+export {
+    userRouter
+}
